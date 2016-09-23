671fc8e3528849b611530f2a286c18c94fa3663d
user <you@example.com>
2016-09-23 09:50:38 +0300

    Modification

--- Lab_1/MathC15/main.c
+++ Lab_1/MathC15/main.c
@@ -4,15 +4,35 @@
 int main()
 {
     float a,x,G,F,Y;
+    int b;
+
     printf("Введите значение переменной а = ");
     scanf("%f" , &a);
     printf("Введите значение переменной x = ");
     scanf("%f", &x);
-    G = (4*a*a+3*a*x+10*x*x)/(15*a*a+29*a*x+12*x*x);
-    F = 1/((float)cos(5*a*a+14*a*x-3*x*x));
-    Y = (float)acos(9*a*a+42*a*x+40*x*x+1);
-    printf("%s%f\n", "G = ", G);
-    printf("%s%f\n", "F = ", F);
-    printf("%s%f\n", "Y = ", Y);
+
+    if(15*a*a+29*a*x+12*x*x == 0)
+        printf("Входные данные не подлежат области определения функции");
+    else {
+        printf("%s\n", "Вычислить функцию G - 1");
+        printf("%s\n", "Вычислить функцию F - 2");
+        printf("%s\n", "Вычислить функцию Y - 3");
+        scanf("%d", &b);
+
+        switch (b) {
+            case 1:
+                G = (4 * a * a + 3 * a * x + 10 * x * x) / (15 * a * a + 29 * a * x + 12 * x * x);
+                printf("%s%f\n","G = ", G);
+                break;
+            case 2:
+                F = 1 / ((float) cos(5 * a * a + 14 * a * x - 3 * x * x));
+                printf("%s%f\n", "F = ", F);
+                break;
+            case 3:
+                Y = (float) acos(9 * a * a + 42 * a * x + 40 * x * x + 1);
+                printf("%s%f\n", "Y = ", Y);
+                break;
+        }
+    }
     return 0;
 }
