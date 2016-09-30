e7040d48b7448eb53340d62e4e944381296c56f6
Dimk000 <dimk0001@gmail.com>
2016-09-30 17:53:03 +0300

    Comeback

---
+++ Lab_3/Examples3/Example5.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void)
+{
+    float x,a;
+    scanf("%f", &x); scanf("%f", &a);
+    do
+    {
+        printf("%f %f\n", x, a);
+        x += 0.5;
+    }
+    while (x > a);
+    return 0;
+}
