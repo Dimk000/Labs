ef2ea6f436fe489cb27216e8108ed851f998f808
user <you@example.com>
2016-10-01 13:52:34 +0300

    Fix

--- Lab_1/MathC15/main.c
+++ Lab_1/MathC15/main.c
@@ -3,20 +3,20 @@
 
 int main()
 {
-    float a,x,G,F,Y;
+    double a,x,G,F,Y;
 
     printf("Введите значение переменной а = ");
-    scanf("%f" , &a);
+    scanf("%lf" , &a);
     printf("Введите значение переменной x = ");
-    scanf("%f", &x);
+    scanf("%lf", &x);
 
     G = (4 * a * a + 3 * a * x + 10 * x * x) / (15 * a * a + 29 * a * x + 12 * x * x);
     printf("%s%f\n","G = ", G);
 
-    F = 1 / ((float) cos(5 * a * a + 14 * a * x - 3 * x * x));
+    F = 1 / (cos(5 * a * a + 14 * a * x - 3 * x * x));
     printf("%s%f\n", "F = ", F);
 
-    Y = (float) acos(9 * a * a + 42 * a * x + 40 * x * x + 1);
+    Y =  acos(9 * a * a + 42 * a * x + 40 * x * x + 1);
     printf("%s%f\n", "Y = ", Y);
 
     return 0;
