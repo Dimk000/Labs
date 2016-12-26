8f8ab6e4eff567a72dd6d192fd12a8aa95a5e982
user <you@example.com>
2016-12-26 15:26:05 +0300

    Made some labs for Python

--- Lab_4/MathPy15/MathPy15.py
+++ Lab_4/MathPy15/MathPy15.py
@@ -1,5 +1,4 @@
 import math
-from matplotlib import pyplot as plt
 
 print("Запустить программу - 1, выйти - 0")
 enter = int(input())
@@ -8,12 +7,13 @@ while enter == 1:
     a = float(input("Введите значение а: "))
     x_from = float(input("Введите значение x от: "))
     x_to = float(input("До: "))
-    steps = float(input("Количество шагов: "))
+    steps = int(input("Количество шагов: "))
     step_value = (x_to-x_from)/steps
     print("Вычислить функцию G - 1")
     print("Вычислить функцию F - 2")
     print("Вычислить функцию Y - 3")
     b = int(input("Выберите функцию: "))
+    counter = 0
     x_values = []
     y_values = []
 
@@ -24,11 +24,17 @@ while enter == 1:
                 print("Входные данные не подлежат области определения функции")
             else:
                 G = (4 * (-18 * a ** 2 + 3 * a * x + 10 * x ** 2)) / (15 * a ** 2 + 29 * a * x + 12 * x ** 2)
-                print("G = ", G)
                 x_values.append(x)
                 y_values.append(G)
             x_from += step_value
-        plt.title('График G')
+        min_array = y_values[counter]
+        max_array = y_values[counter]
+        while counter < steps:
+            if y_values[counter] < min_array:
+                min_array = y_values[counter]
+            if y_values[counter] > max_array:
+                max_array = y_values[counter]
+            counter += 1
     elif b == 2:
         while x_from < x_to:
             x = x_from
@@ -36,11 +42,17 @@ while enter == 1:
                 print("Входные данные не подлежат области определения функции")
             else:
                 F = 1 / (math.cos(5 * a ** 2 + 14 * a * x - 3 * x ** 2))
-                print("F = ", F)
                 x_values.append(x)
                 y_values.append(F)
             x_from += step_value
-        plt.title('График F')
+        min_array = y_values[counter]
+        max_array = y_values[counter]
+        while counter < steps:
+            if y_values[counter] < min_array:
+                min_array = y_values[counter]
+            if y_values[counter] > max_array:
+                max_array = y_values[counter]
+            counter += 1
     elif b == 3:
         while x_from < x_to:
             x = x_from
@@ -48,19 +60,29 @@ while enter == 1:
                 print("Входные данные не подлежат области определения функции")
             else:
                 Y = math.acosh(9 * a ** 2 + 42 * a * x + 40 * x ** 2 + 1)
-                print("Y = ", Y)
                 x_values.append(x)
                 y_values.append(Y)
             x_from += step_value
-        plt.title('График Y')
+        min_array = y_values[counter]
+        max_array = y_values[counter]
+        while counter < steps:
+            if y_values[counter] < min_array:
+                min_array = y_values[counter]
+            if y_values[counter] > max_array:
+                max_array = y_values[counter]
+            counter += 1
     else:
         print("Ошибка")
         break
-    plt.plot(x_values, y_values, 'bo-')
-    plt.grid(True)
-    plt.xlabel('X')
-    plt.ylabel('Y')
-    plt.show()
+    print("________________")
+    print("|   X   |   Y   |")
+    print("----------------")
+    counter = 0
+    while counter < steps:
+        print("|{}|{}|".format(x_values[counter], y_values[counter]))
+        counter += 1
+    print("Наибольший элемент массива = ", max_array)
+    print("Наименьший элемент массива = ", min_array)
 
     print("Запустить программу - 1, выйти - 0")
     enter = int(input())
--- Lab_7/MathPy15/MathPy15.py
+++ Lab_7/MathPy15/MathPy15.py
@@ -1,66 +1,74 @@
 import math
-from matplotlib import pyplot as plt
 
 print("Запустить программу - 1, выйти - 0")
 enter = int(input())
 
 while enter == 1:
-    a = float(input("Введите значение а: "))
-    x_from = float(input("Введите значение x от: "))
-    x_to = float(input("До: "))
-    steps = float(input("Количество шагов: "))
-    step_value = (x_to-x_from)/steps
+    string_input = input("Введите значение а от: ")
+    a_from = float(string_input)
+    string_input = input("До: ")
+    a_to = float(string_input)
+    string_input = input("Введите значение x от: ")
+    x_from = float(string_input)
+    string_input = input("До: ")
+    x_to = float(string_input)
+    string_input = input("Количество шагов: ")
+    steps = int(string_input)
+    x_step_value = (x_to-x_from)/steps
+    a_step_value = (a_to-a_from)/steps
     print("Вычислить функцию G - 1")
     print("Вычислить функцию F - 2")
     print("Вычислить функцию Y - 3")
     b = int(input("Выберите функцию: "))
-    x_values = []
+    counter = 0
     y_values = []
 
     if b == 1:
         while x_from < x_to:
             x = x_from
+            a = a_from
             if 15*a**2+29*a*x+12*x**2 >= -0.00001 and 15*a**2+29*a*x+12*x**2 <= 0.00001:
                 print("Входные данные не подлежат области определения функции")
             else:
                 G = (4 * (-18 * a ** 2 + 3 * a * x + 10 * x ** 2)) / (15 * a ** 2 + 29 * a * x + 12 * x ** 2)
-                print("G = ", G)
-                x_values.append(x)
                 y_values.append(G)
-            x_from += step_value
-        plt.title('График G')
+            x_from += x_step_value
+            a_from += a_step_value
     elif b == 2:
         while x_from < x_to:
             x = x_from
+            a = a_from
             if math.cos(5*a**2+14*a*x-3*x**2) >= -0.00001 and math.cos(5*a**2+14*a*x-3*x**2) <= 0.00001:
                 print("Входные данные не подлежат области определения функции")
             else:
                 F = 1 / (math.cos(5 * a ** 2 + 14 * a * x - 3 * x ** 2))
-                print("F = ", F)
-                x_values.append(x)
                 y_values.append(F)
-            x_from += step_value
-        plt.title('График F')
+            x_from += x_step_value
+            a_from += a_step_value
     elif b == 3:
         while x_from < x_to:
             x = x_from
+            a = a_from
             if (9 * a ** 2 + 42 * a * x + 40 * x ** 2 + 1) <= 1:
                 print("Входные данные не подлежат области определения функции")
             else:
                 Y = math.acosh(9 * a ** 2 + 42 * a * x + 40 * x ** 2 + 1)
-                print("Y = ", Y)
-                x_values.append(x)
                 y_values.append(Y)
-            x_from += step_value
-        plt.title('График Y')
+            x_from += x_step_value
+            a_from += a_step_value
     else:
         print("Ошибка")
         break
-    plt.plot(x_values, y_values, 'bo-')
-    plt.grid(True)
-    plt.xlabel('X')
-    plt.ylabel('Y')
-    plt.show()
+    file = open("/home/user/Labs/Lab_7/MathPy15/text.txt", "w")
+    while counter < steps:
+        file.write(str(y_values[counter])+"\n")
+        counter += 1
+    file.close()
+    y_values.clear()
+    file = open("/home/user/Labs/Lab_7/MathPy15/text.txt", "r")
+    for line in file:
+        print(line)
+    file.close()
 
     print("Запустить программу - 1, выйти - 0")
     enter = int(input())
