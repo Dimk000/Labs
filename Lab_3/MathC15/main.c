e42eac2acfa86bacf8ac74f9f71b090f65347d0e
user <you@example.com>
2016-12-27 21:32:53 +0300

    Add adaptive step for Lab 3

--- Lab_3/MathC15/main.c
+++ Lab_3/MathC15/main.c
@@ -6,8 +6,7 @@ double acosh(double);
 
 int main()
 {
-    double a,x,G,F,Y,z,from,to,i;
-    int steps;
+    double a,x,G,G1,F,F1,Y,Y1,from,to,i,step_value,difference;
     char b;
 
     for(;;){
@@ -17,15 +16,16 @@ int main()
     scanf("%s", yes2);
     if (strcmp(yes2,yes1) == 0){
 
-    printf("Введите значение переменной а = ");
+    printf("Введите значение переменной а: ");
     scanf("%lf" , &a);
-    printf("Введите значение переменной x от:  ");
+    printf("Введите значение переменной x от: ");
     scanf("%lf", &from);
     printf("До: ");
     scanf("%lf", &to);
-    printf("Количество шагов: ");
-    scanf("%i", &steps);
-    z = (to-from)/steps;
+    printf("Размер шага: ");
+    scanf("%lf", &step_value);
+    printf("Введите разницу между функциями: ");
+    scanf("%lf", &difference);
 
         printf("%s\n", "Вычислить функцию G, F, Y");
         printf("%s", "Выберите функцию: ");
@@ -33,7 +33,7 @@ int main()
 
         switch (b) {
             case 'G':
-                for(i=from; i<to;i+=z){
+                for(i=from; i<to;i+=step_value){
                     x=i;
                 if (((15*a*a+29*a*x+12*x*x) >= -0.00001
                      && (15*a*a+29*a*x+12*x*x) <= 0.00001)){
@@ -41,10 +41,17 @@ int main()
                     break;}
                 else{
                     G = (4 * a * a + 3 * a * x + 10 * x * x) / (15 * a * a + 29 * a * x + 12 * x * x);
-                    printf("%s%f\n","G = ", G);}}
+                    printf("%s%f\n","G = ", G);}
+                G1 = (4 * a * a + 3 * a * (x+step_value) + 10 * (x+step_value) * (x+step_value))
+                     / (15 * a * a + 29 * a * (x+step_value) + 12 * (x+step_value) * (x+step_value));
+                if (fabs(G1-G) < difference){
+                    while(fabs(G1-G) < difference){
+                    step_value*=2;
+                    G1 = (4 * a * a + 3 * a * (x+step_value) + 10 * (x+step_value) * (x+step_value))
+                         / (15 * a * a + 29 * a * (x+step_value) + 12 * (x+step_value) * (x+step_value));}}}
                     break;
             case 'F':
-                for(i=from;i<to;i+=z){
+                for(i=from;i<to;i+=step_value){
                     x=i;
                 if ((cos(5 * a * a + 14 * a * x - 3 * x * x)) >= -0.00001
                     && (cos(5 * a * a + 14 * a * x - 3 * x * x)) <= 0.00001){
@@ -52,17 +59,27 @@ int main()
                     break;}
                 else{
                     F = 1 / (cos(5 * a * a + 14 * a * x - 3 * x * x));
-                    printf("%s%f\n", "F = ", F);}}
+                    printf("%s%f\n", "F = ", F);}
+                F1 = 1 / (cos(5 * a * a + 14 * a * (x+step_value) - 3 * (x+step_value) * (x+step_value)));
+                if (fabs(F1-F)<difference){
+                    while(fabs(F1-F)<difference){
+                        step_value*=2;
+                        F1 = 1 / (cos(5 * a * a + 14 * a * (x+step_value) - 3 * (x+step_value) * (x+step_value)));}}}
                     break;
             case 'Y':
-                for(i=from;i<to;i+=z){
+                for(i=from;i<to;i+=step_value){
                     x=i;
                 if ((9 * a * a + 42 * a * x + 40 * x * x + 1) <= 1){
                     printf("Входные данные не подлежат области определения функции");
                     break;}
                 else{
                     Y = acosh(9 * a * a + 42 * a * x + 40 * x * x + 1);
-                    printf("%s%f\n", "Y = ", Y);}}
+                    printf("%s%f\n", "Y = ", Y);}
+                Y1 = acosh(9 * a * a + 42 * a * (x+step_value) + 40 * (x+step_value) * (x+step_value) + 1);
+                if(fabs(Y1-Y)<difference)
+                    while(fabs(Y1-Y)<difference){
+                        step_value*=2;
+                        Y1 = acosh(9 * a * a + 42 * a * (x+step_value) + 40 * (x+step_value) * (x+step_value) + 1);}}
                     break;
             default:
                 printf("%s\n","Ошибка");
