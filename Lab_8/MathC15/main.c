83b68c1f046aa3856989d50a778d0c87f275e219
user <you@example.com>
2016-12-29 11:35:00 +0300

    All labs are complete

---
+++ Lab_8/MathC15/main.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+#include <omp.h>
+
+struct points
+{
+    double x[100];
+    double y[100];
+};
+
+struct main_point
+{
+    double main_x;
+    double main_y;
+};
+
+struct points generator_array(void)
+{
+    struct points array;
+    int i = 0;
+    srand(time(NULL));
+
+    for (i = 0; i < 100; i++) {
+        array.x[i] = (double) rand() / RAND_MAX * (5.0 + 5.0) - 5.0;
+        array.y[i] = (double) rand() / RAND_MAX * (5.0 + 5.0) - 5.0;
+    }
+    return array;
+}
+
+int belongOrNot(struct main_point one_p, struct main_point all_p,double rad)
+{
+    double distance;
+    distance = sqrt((pow(2,one_p.main_x-all_p.main_x))+pow(2,one_p.main_y-all_p.main_y));
+    if (distance <= rad)
+        return 1;
+    else
+        return 0;
+}
+int amountOfPoints(double rad)
+{
+    struct main_point thePoint, allPoints;
+    struct points mass;
+    int counter = 0;
+    int i = 0, choice = 0;
+    srand(time(NULL));
+
+    choice = rand() % 100;
+
+    mass = generator_array();
+    mass.x[choice] = thePoint.main_x;
+    mass.y[choice] = thePoint.main_y;
+
+    for (i = 0; i < 100; i++)
+    {
+        allPoints.main_x = mass.x[i];
+        allPoints.main_y = mass.y[i];
+        if (belongOrNot(thePoint,allPoints,rad) == 1)
+        {
+            counter++;
+        }
+    }
+    if (counter != 0)
+    {
+        counter = counter - 1;
+    }
+    return counter;
+}
+
+int main()
+{
+    FILE* output;
+    double radius,start,end;
+
+    printf("Введите радиус окрестности: ");
+    scanf("%lf", &radius);
+    start = omp_get_wtime();
+    printf("Количество точек в заданной окрестности: %d\n", amountOfPoints(radius));
+    end = omp_get_wtime();
+    printf("Время выполнения программы: %lf\n", end - start);
+    output = fopen("/home/user/Labs/Lab_8/MathC15/out.txt","a");
+    fprintf(output,"%lf\n",end - start);
+    fclose(output);
+    return 0;
+}
